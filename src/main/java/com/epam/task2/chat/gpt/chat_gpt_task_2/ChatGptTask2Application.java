7e47c0257440ddef64d937675f266d02284a2d56
Stanislav Donetc <stanislavdonetc@gmail.com>
2024-11-06 14:28:23 +0200

    initial commit

---
+++ src/main/java/com/epam/task2/chat/gpt/chat_gpt_task_2/ChatGptTask2Application.java
@@ -0,0 +1,13 @@
+package com.epam.task2.chat.gpt.chat_gpt_task_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatGptTask2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatGptTask2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/epam/task2/chat/gpt/chat_gpt_task_2/ChatGptTask2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.task2.chat.gpt.chat_gpt_task_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatGptTask2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
